d33e8e651fac671e0b6a289a7845dbfaf08a8b10
LieRabbit <lierabbitxyy@gmail.com>
2018-01-22 13:02:05 +0800

    添加注释

--- src/main/java/lierabbit/thymeleaf/ThymeleafCtrl.java
+++ src/main/java/lierabbit/thymeleaf/ThymeleafCtrl.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller//这是一个控制器
 public class ThymeleafCtrl
 {
     @RequestMapping("/")
     public String hello(Model model)
     {
-        model.addAttribute("hello","hello thymeleaf");
-        return "hello";
+        model.addAttribute("hello","hello thymeleaf");//添加一个值为"hello thymeleaf"的hello变量到视图
+        return "hello";//在templates下寻找hello.html
     }
 }
